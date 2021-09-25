dd44b7249410361535c197709970b2ccd83a01a7
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2021-09-25 06:17:06 -0500

    ready

--- plants/lib/pages/home.page.dart
+++ plants/lib/pages/home.page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:plants/models/plant.model.dart';
+import 'package:plants/pages/list.page.dart';
+import 'package:plants/pages/main.page.dart';
+import 'package:plants/pages/map.page.dart';
 import 'package:plants/providers/plants.provider.dart';
 import 'package:plants/widgets/bottom.navigation.widget.dart';
 import 'package:plants/widgets/navigation.drawer.widget.dart';
@@ -21,8 +24,19 @@ class _HomePageState extends State<HomePage> {
     futurePlants = plantsProvider.getAllPlants(page: 1);
     super.initState();
   }
-
   @override
+  int indexTap=0;
+  final List<Widget> widgetsChildren=[
+    Principal(),
+    Listado(),
+    MapPage(),
+  ];
+  void onTapTapped(int index ){
+    setState((){
+      indexTap= index;
+    }
+    );
+  }
   Widget build(BuildContext context) {
     return FutureBuilder(
         future: futurePlants,
@@ -38,15 +52,40 @@ class _HomePageState extends State<HomePage> {
             listPlants.add(item);
           });
           return Scaffold(
-              drawer: const NavigationDrawer(),
-              appBar: AppBar(
-                backgroundColor: Colors.green,
-                title: const Text("Inicio"),
+            drawer: const NavigationDrawer(),
+            appBar: AppBar(
+              backgroundColor: Colors.green,
+              title: const Text("Planta Biodiversa"),
+            ),
+            bottomNavigationBar: Theme(
+              data:  Theme.of(context).copyWith(
+                canvasColor: Colors.white,
+                primaryColor: Colors.purple,
               ),
-              bottomNavigationBar: BottomNavigation(plants: listPlants),
-              body: const Center(
-                child: Text("Hola mundo"),
-              ));
+              child: BottomNavigationBar(
+                  onTap:onTapTapped,
+                  currentIndex: indexTap,
+                  items: const [
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: "inicio",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.list),
+                      label: "listado",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.map),
+                      label: "mapa",
+                    ),
+
+
+                  ]
+              ),
+            ),
+            body: widgetsChildren[indexTap],
+          );
+
         });
   }
 }
