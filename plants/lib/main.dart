885fb4a486f643078ec99619c932b66c339051ac
Diego Alejandro Asencio Cuellar <diegoasencio96@gmail.com>
2021-09-25 00:54:55 -0500

    add: project structure base

---
+++ plants/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:plants/pages/home.page.dart';
+
+void main() {
+  runApp(const PlantaBiodiversaApp());
+}
+
+class PlantaBiodiversaApp extends StatelessWidget {
+  const PlantaBiodiversaApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Planta Biodiversa',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ plants/lib/pages/home.page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int pages = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text("Inicio"),
+        ),
+        body: Center(
+          child: Container(
+            child: const Text("Hola mundo"),
+          ),
+        )
+    );
+  }
+
+}
