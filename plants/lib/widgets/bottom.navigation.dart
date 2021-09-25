fba8387268e3fea24ec5dbde220992c149c0ecb0
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2021-09-25 03:47:20 -0500

    change color navigationbottom

--- plants/lib/widgets/bottom.navigation.dart
+++ plants/lib/widgets/bottom.navigation.dart
@@ -6,24 +6,24 @@ class BottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      backgroundColor: Colors.green,
+
 
       type: BottomNavigationBarType.fixed,
       selectedLabelStyle: TextStyle(fontSize: 12,color: Colors.white),
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
-          icon: Icon(Icons.home, color: Colors.white),
+          icon: Icon(Icons.home, color: Colors.green),
           backgroundColor: Colors.green,
           label: 'Inicio',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.list, color: Colors.white),
+          icon: Icon(Icons.list, color: Colors.green),
           backgroundColor: Colors.green,
           label: 'Lista',
 
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.map,  color: Colors.white),
+          icon: Icon(Icons.map,  color: Colors.green),
 
           label: 'Mapa',
         ),
